936bac8120aab15146190833deb0f83cd2e6f0e5
samtenna <samueltennant@hotmail.com>
2023-11-04 17:35:05 +0000

    idk

--- client/src/lib/common/HeaderWork.svelte
+++ client/src/lib/common/HeaderWork.svelte
@@ -1,47 +1,76 @@
 <script lang="ts">
-    import Searchbar from "./Searchbar.svelte";
+  import Searchbar from "./Searchbar.svelte";
 </script>
 
 <div class="bg-primary text-white px-12 py-4">
-    <div class="flex items-center justify-between">
-        <img src="../../../../whale.png" alt="logo">
+  <div class="flex items-center justify-between">
+    <img src="../../../../whale.png" alt="logo" />
 
-        <Searchbar />
-        
-        <ul class="flex items-center justify-between gap-4">
-            <li>
-                <a href="/user/home">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                    </svg>
-                </a>
+    <Searchbar />
 
-            </li>
-            <li>
-                <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                    </svg>
-                </a>
-
-            </li>
-            <li>
-                <a href="/user/login">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                    </svg>
-                </a>
-
-            </li>
-        </ul>
-    </div>
-    <div class="my-4">
-        <ul class="mx-12 flex justify-between items-center gap-4">
-            <li class="border-l-secondary"><a href="/">Home</a></li>
-            <li class="border-l-secondary"><a href="/music">Music</a></li>
-            <li class="border-l-secondary"><a href="">New Release</a></li>
-            <li><a href="">Saved</a></li>
-        </ul>
-    </div>
+    <ul class="flex items-center justify-between gap-4">
+      <li>
+        <a href="/user/home">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="/user/dashboard">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="my-4">
+    <ul class="mx-12 flex justify-between items-center gap-4">
+      <li class="border-l-secondary"><a href="/">Home</a></li>
+      <li class="border-l-secondary"><a href="/music">Music</a></li>
+      <li class="border-l-secondary"><a href="">New Release</a></li>
+      <li><a href="">Saved</a></li>
+    </ul>
+  </div>
 </div>
-
