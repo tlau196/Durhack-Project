f87b9cb991e2a29170cf6885ca0b8a09ddba4cba
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-11-04 17:25:39 +0000

    Music page

---
+++ client/src/routes/music/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Footer from "$lib/common/Footer.svelte";
+    import HeaderWork from "$lib/common/HeaderWork.svelte"
+    // import FilterBox from "$lib/common/HeaderWork.svelte"
+</script>
+
+<div>
+    <HeaderWork />
+
+    <h2 class="text-5xl font-bold p-12">Music</h2>
+    <div class="flex items-center justify-center">
+        <!-- <FilterBox />  -->
+        <div class="grid grid-cols-3 gap-96">
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+        </div>
+    </div>
+
+    <Footer />
+</div>
