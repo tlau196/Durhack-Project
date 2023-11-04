438c1680289dd089615c81f4a699d97e39606034
tlau196 <kgpv84@durham.ac.uk>
2023-11-04 13:51:51 +0000

    Added firebasae config

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 import { initializeApp } from 'firebase/app';
-//insert firebase config here
-//
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
+    apiKey: "AIzaSyBXOjJy_EpIafw6mntPfHjMq9iKPknx_4g",
+    authDomain: "durhack-project-45a1b.firebaseapp.com",
+    projectId: "durhack-project-45a1b",
+    storageBucket: "durhack-project-45a1b.appspot.com",
+    messagingSenderId: "912269921938",
+    appId: "1:912269921938:web:017b70dda6b2c8eb06e04b",
+    measurementId: "G-MWCQ2PH5CM"
 }
 
 const app = initializeApp(firebaseConfig);
