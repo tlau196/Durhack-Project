b32627cc708b2d4d90e103eda26b5a34e0ed67e8
samtenna <samueltennant@hotmail.com>
2023-11-04 17:24:36 +0000

    more in dashboard

--- client/src/routes/user/dashboard/+page.svelte
+++ client/src/routes/user/dashboard/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import Button, { ButtonBackgroundColour } from "$lib/common/Button.svelte";
+  import { firebaseAuth } from "$lib/firebase";
   import { authStore } from "$lib/stores";
-  import type { User } from "firebase/auth";
+  import { signOut, type User } from "firebase/auth";
 
   let user: User;
 
@@ -17,16 +20,30 @@
     class="bg-neutral-200 p-8 transition duration-200 hover:bg-neutral-300 col-span-1 rounded-md"
   >
     <h2 class="text-2xl">
-      Hello, <span class="font-semibold">{user.displayName}</span>.
+      Hello, <span class="font-semibold">{user?.displayName}</span>.
     </h2>
+    <Button
+      onClick={() => {
+        signOut(firebaseAuth);
+        goto("/");
+      }}
+      backgroundColour={ButtonBackgroundColour.Red}
+      darkText={false}>Sign Out</Button
+    >
   </div>
   <div
     class="bg-neutral-200 p-8 transition duration-200 hover:bg-neutral-300 col-span-2 rounded-md"
-  />
+  >
+    <h2 class="text-3xl">Your Products</h2>
+  </div>
   <div
     class="bg-neutral-200 p-8 transition duration-200 hover:bg-neutral-300 col-span-2 rounded-md"
-  />
+  >
+    <h2 class="text-3xl">Stonks</h2>
+  </div>
   <div
     class="bg-neutral-200 p-8 transition duration-200 hover:bg-neutral-300 col-span-1 rounded-md"
-  />
+  >
+    <h2 class="text-3xl">Purchase History</h2>
+  </div>
 </div>
