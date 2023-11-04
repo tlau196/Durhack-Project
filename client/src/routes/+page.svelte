fd73808fdd23b62d36cce49278d1b4b057707cb2
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-11-04 14:01:05 +0000

    header

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,12 +1,19 @@
+<script lang="ts">
+    import Header from "$lib/common/HeaderWork.svelte"
+</script>
+
+
+
 <svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-  <h1>Hello, world!</h1>
+    <Header /> 
+    <h1>Hello, world!</h1>
 
-  <h2 class="text-red-600">
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
+    <h2 class="text-red-600">
+        try editing <strong>src/routes/+page.svelte</strong>
+    </h2>
 </section>
