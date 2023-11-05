038fa10bcd54ea6e7a26c57a0f4e5babc5b29da9
samtenna <samueltennant@hotmail.com>
2023-11-05 00:34:02 +0000

    added removing from basket

--- client/src/routes/basket/BasketItem.svelte
+++ client/src/routes/basket/BasketItem.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+  import Button, { ButtonBackgroundColour } from "$lib/common/Button.svelte";
   import type { Listing } from "$lib/firebase";
+  import { basketStore } from "$lib/stores";
+
+  function handleRemoveFromBasket() {
+    basketStore.update((basket) => {
+      const index = basket.findIndex((item) => item.id === listing.id);
+      basket.splice(index, 1);
+      return basket;
+    });
+  }
 
   export let listing: Listing;
 </script>
@@ -11,5 +21,11 @@
   <div class="flex-1 flex items-start justify-between flex-col">
     <h2 class="text-3xl">Name: {listing.product_name}</h2>
     <h2 class="text-3xl">Price: {listing.price}</h2>
+
+    <Button
+      backgroundColour={ButtonBackgroundColour.Red}
+      darkText={false}
+      onClick={handleRemoveFromBasket}>Remove from basket</Button
+    >
   </div>
 </div>
