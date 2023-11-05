034c9ea8583834fabf935d8d92d6fe7b14fa5d62
tlau196 <kgpv84@durham.ac.uk>
2023-11-05 08:21:02 +0000

    centred title

--- client/src/routes/music/+page.svelte
+++ client/src/routes/music/+page.svelte
@@ -44,7 +44,7 @@
 <div class="px-12">
     
     {#if productsAndImages}
-        <h2 class="text-5xl font-bold p-12">Albums</h2>
+        <h2 class="text-5xl font-bold p-12, text-center">Albums</h2>
         <div class="flex items-center justify-center">
             <!-- <FilterBox />  -->
             <div class="text-center px-12 py-4 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-20">
